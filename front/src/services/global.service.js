f7a0a44edeb83ca83449a0c30522b92f2e22aaf9
ErwSim <erwan@dassim.fr>
2021-11-29 16:44:14 +0100

    Ajout de méthodes dans le service global

--- front/src/services/global.service.js
+++ front/src/services/global.service.js
@@ -12,6 +12,9 @@ export class GlobalService {
     this.authInterceptor();
   }
 
+  /**
+   * Add authorization token to identified users
+   */
   authInterceptor() {
     this.api.interceptors.request.use((config) => {
       const token = JSON.parse(localStorage.getItem("user"))?.token;
@@ -21,5 +24,43 @@ export class GlobalService {
       }
       return config;
     });
+  }
+
+  /**
+   * Get all elements of a model
+   * @returns {Promise<T>[]}
+   */
+  async getAll() {
+    try {
+      return await this.api.get();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * Get one elemnt of a model by its id
+   * @param {number} id - The searched id
+   * @returns {Promise<T>}
+   */
+  async getOneById(id) {
+    try {
+      return await this.api.get(id);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * Create one element of a model
+   * @param {T} payload - The required payload
+   * @returns {Promise<T>} - The created element
+   */
+  async create(payload) {
+    try {
+      return await this.api.post(payload);
+    } catch (e) {
+      throw e;
+    }
   }
 }
